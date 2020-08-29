e4cda929cb8ba2159ccfcccbbe4a74f3d77fa83e
iamBishwa <bishwa1996@gmail.com>
2020-08-29 21:42:42 +0800

    added two files

---
+++ app.js
@@ -0,0 +1,12 @@
+/*function sayHello(name){
+    console.log('Hello'+ name);
+}*/
+// sayHello('Bishwa');
+// console.log(window);    ----->undefined
+
+// console.log(); ----------->//global object
+// var message();  ----------> local object
+
+const log = require('./logger');
+// console.log(logger)
+log('message');
